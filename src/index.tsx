3d4f0d4ad82fd223d1798d36b2fc435a262fe817
DmitriyBarhonov <explay167@gmail.com>
2023-09-14 18:42:09 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './app/App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
-
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -12,4 +11,3 @@ root.render(
 		<App/>
 	</Provider>
 );
-   
