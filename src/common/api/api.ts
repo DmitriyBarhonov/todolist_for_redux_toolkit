b95913f916f5f84dd5a67b8e201de2ae1fcaf03c
Dmitriy <explay167@gmail.com>
2023-11-15 14:39:07 +0300

    fix cors

--- src/common/api/api.ts
+++ src/common/api/api.ts
@@ -2,7 +2,11 @@ import axios from 'axios'
 
 export const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+    withCredentials: true,
     headers: {
-        'API-KEY': '1cdd9f77-c60e-4af5-b194-659e4ebd5d41'
+        'API-KEY': '1cdd9f77-c60e-4af5-b194-659e4ebd5d41',
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, POST, OPTION",
     }
 })
