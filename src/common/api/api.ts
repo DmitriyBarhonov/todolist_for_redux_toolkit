d9e34b3d919cd2d6c31950681acc9be419b2dc71
Dmitriy <explay167@gmail.com>
2023-11-21 19:06:36 +0300

    add deploy script

--- src/common/api/api.ts
+++ src/common/api/api.ts
@@ -1,11 +1,12 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
-    headers: {
-        'API-KEY': '1cdd9f77-c60e-4af5-b194-659e4ebd5d41',
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Methods": "GET, POST, OPTION",
-    }
-})
+  baseURL: "https://social-network.samuraijs.com/api/1.1/",
+  withCredentials: true,
+  headers: {
+    "API-KEY": "1cdd9f77-c60e-4af5-b194-659e4ebd5d41",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "GET, POST, OPTION",
+  },
+});
