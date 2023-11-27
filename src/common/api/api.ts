941c1a11ec7e02f139f8fc2456f3efbbdb76f244
Dmitriy <explay167@gmail.com>
2023-11-27 17:52:34 +0300

    fix api

--- src/common/api/api.ts
+++ src/common/api/api.ts
@@ -5,8 +5,5 @@ export const instance = axios.create({
   withCredentials: true,
   headers: {
     "API-KEY": "1cdd9f77-c60e-4af5-b194-659e4ebd5d41",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "Content-Type",
-    "Access-Control-Allow-Methods": "GET, POST, OPTION",
   },
 });
